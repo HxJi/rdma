22485917274775ed2dd2565212ff9e6b7b72339b
hj14@bmp-1 <hj14@illinois.edu>
2022-01-18 14:24:39 +0000

    move dev to local

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <inttypes.h>
+
+struct config_info{
+    bool is_server;
+}
+int main(int argc, char *argv[]){
+
+}
