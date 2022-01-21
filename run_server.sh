0038336f787965beeafc40658d6f3355b140b4d5
hj14@bmp-1 <hj14@illinois.edu>
2022-01-21 21:29:40 +0000

    infinite server

--- run_server.sh
+++ run_server.sh
@@ -2,10 +2,10 @@
 
 make && sudo make install && sudo modprobe rdma_krping debug=1 &&
 
-for i in {0..10}
+while :
     do 
-        echo "Server run $i times"
-        echo "server,addr=192.168.101.14,port=20886,verbose,validate,verbose" >/proc/krping
+        # echo "Server run $i times"
+        echo "server,addr=192.168.101.14,port=20886,verbose,validate" >/proc/krping
     done
 
 # echo "server,addr=192.168.101.14,port=20886,verbose,validate,verbose" >/proc/krping
