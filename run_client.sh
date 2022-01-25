c437baf286aaa4d132de7c3a52ca0106ea56bc24
hj14@bmp-1 <hj14@illinois.edu>
2022-01-25 21:05:15 +0000

    update count

--- run_client.sh
+++ run_client.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 
 make && sudo make install && sudo modprobe rdma_krping debug=1 &&
-while :
-      do 
-        # echo "Server run $i times"
-        echo "client,addr=192.168.101.14,port=20886,count=2,validate,verbose" >/proc/krping
-    done
+echo "client,addr=192.168.101.14,port=20886,count=100,validate,verbose" >/proc/krping
+# while :
+#       do 
+#         # echo "Server run $i times"
+#         echo "client,addr=192.168.101.14,port=20886,count=2,validate,verbose" >/proc/krping
+#     done
 
--- run_server.sh
+++ run_server.sh
@@ -2,11 +2,11 @@
 
 make && sudo make install && sudo modprobe rdma_krping debug=1 &&
 
-while :
-      do 
-        # echo "Server run $i times"
-        echo "server,addr=192.168.101.14,port=20886,verbose,validate" >/proc/krping
-    done
+# while :
+#       do 
+#         # echo "Server run $i times"
+#         echo "server,addr=192.168.101.14,port=20886,verbose,validate" >/proc/krping
+#     done
 
 # for i in {0..10}:
-# echo "server,addr=192.168.101.14,port=20886,verbose,validate,verbose" >/proc/krping
+echo "server,addr=192.168.101.14,port=20886,verbose,validate,verbose" >/proc/krping
