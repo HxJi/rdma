c1a517b0354116c637217031e229dc7a97823830
hj14@bmp-1 <hj14@illinois.edu>
2022-05-31 14:04:19 +0000

    update client script

--- run_client.sh
+++ run_client.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 make && sudo make install && sudo modprobe rdma_krping debug=0 &&
-echo "client,addr=192.168.101.14,port=20886,count=5000" >/proc/krping
+echo "client,addr=192.168.101.12,port=20886,count=5000" >/proc/krping
 # while :
 #       do 
 #         # echo "Server run $i times"
